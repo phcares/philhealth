d9bd39a85ac5b59c062b613d2e68b9142d27210a
phcares <34857681+phcares@users.noreply.github.com>
2018-01-04 16:12:00 +0800

    Update page.js

--- assets/js/page.js
+++ assets/js/page.js
@@ -6,7 +6,14 @@ $(".button-collapse").sideNav();
     getAllArchivedAdmins();
     getAllArchivedCares();
     getAllUserReports();
-    $('#reportsTable').DataTable();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: false // Close upon selecting a date,
+      });
     $('.modal').modal();
     $('select').material_select();
 } );
